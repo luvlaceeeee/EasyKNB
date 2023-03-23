d29a18fdfbd9c46fdd43b18e50834f0ca613d7a3
luvlaceeeee <nikitan2002@gmail.com>
2023-03-23 05:44:58 +0300

    added sidebar opening

--- src/modules/Sidebar/components/Sidebar.tsx
+++ src/modules/Sidebar/components/Sidebar.tsx
@@ -1,15 +1,15 @@
 import { atom, useAtom } from 'jotai';
 import SidebarHeader from './SidebarHeader';
 
-export const isSidebarOpen = atom(true);
+export const isSidebarOpen = atom(false);
 
 const Sidebar = () => {
-  const [isOpen, setOpen] = useAtom(isSidebarOpen);
+  const [isOpen] = useAtom(isSidebarOpen);
   return (
     <div
       className={`${
         isOpen ? 'w-60' : 'w-28'
-      } absolute top-0 left-0 bottom-0 border-r-2 border-zinc-400 border-opacity-20 bg-white p-6 transition-all duration-300 ease-out`}
+      } absolute top-0 left-0 bottom-0 border-r-2 border-zinc-400 border-opacity-20 bg-white p-6 transition-all duration-500 ease-out`}
     >
       <SidebarHeader />
     </div>
--- src/modules/Sidebar/components/SidebarHeader.tsx
+++ src/modules/Sidebar/components/SidebarHeader.tsx
@@ -1,15 +1,28 @@
+import { useAtom } from 'jotai';
 import { FC } from 'react';
-import { FiAlignLeft } from 'react-icons/fi';
+import { FiAlignJustify, FiAlignLeft } from 'react-icons/fi';
 import IconButton from '../../../shared/UI/Buttons/IconButton';
+import { isSidebarOpen } from './Sidebar';
 
 const SidebarHeader: FC = () => {
+  const [isOpen, setOpen] = useAtom(isSidebarOpen);
   return (
     <div className="flex w-full items-center justify-between text-lg">
-      <span className="font-bold">Jirello</span>
-      <IconButton
-        icon={<FiAlignLeft size={20} color="black" />}
-        handlerFn={() => console.log('hello')}
-      />
+      <div className="pl-2">
+        <IconButton
+          icon={
+            isOpen ? <FiAlignJustify size={20} /> : <FiAlignLeft size={20} />
+          }
+          handlerFn={() => setOpen(!isOpen)}
+        />
+      </div>
+      <span
+        className={`${
+          isOpen ? 'opacity-100' : 'opacity-0'
+        } font-bold transition-opacity duration-300 ease-out`}
+      >
+        Jirello
+      </span>
     </div>
   );
 };
