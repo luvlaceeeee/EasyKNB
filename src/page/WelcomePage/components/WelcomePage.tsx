81276da4f579e010c787d9a10e9096baec89d6ec
luvlaceeeee <nikitan2002@gmail.com>
2023-03-24 05:19:43 +0300

    change welcomepage

--- src/page/WelcomePage/components/WelcomePage.tsx
+++ src/page/WelcomePage/components/WelcomePage.tsx
@@ -1,7 +1,17 @@
 import { FC } from 'react';
 
 const WelcomePage: FC = () => {
-  return <div>WelcomePage</div>;
+  const handlerClick = () => {
+    localStorage.setItem('userId', '1');
+  };
+
+  return (
+    <div>
+      <button onClick={handlerClick} className="m-10 bg-slate-400 p-5">
+        State user id to local storage
+      </button>
+    </div>
+  );
 };
 
 export default WelcomePage;
