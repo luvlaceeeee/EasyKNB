430faddfbc46ce9b1c51e632d1f3b44937d33027
luvlaceeeee <nikitan2002@gmail.com>
2023-03-27 14:53:03 +0300

    change header layout

--- src/shared/layout/HeaderLayout.tsx
+++ src/shared/layout/HeaderLayout.tsx
@@ -2,7 +2,11 @@ import { FC } from 'react';
 import { LayoutProps } from './layoutTypes';
 
 const HeaderLayout: FC<LayoutProps> = ({ children }) => {
-  return <div className="shrink-0 basis-auto">{children}</div>;
+  return (
+    <div className="h-1/5 shrink-0 basis-auto border-b-2 border-zinc-400 border-opacity-20">
+      {children}
+    </div>
+  );
 };
 
 export default HeaderLayout;
