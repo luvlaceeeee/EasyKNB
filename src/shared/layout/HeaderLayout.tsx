9c1b4bf9e2cadfb1d6d659cf8e08c7a23e74b6f9
luvlaceeeee <nikitan2002@gmail.com>
2023-03-27 16:15:28 +0300

    change headerlayout

--- src/shared/layout/HeaderLayout.tsx
+++ src/shared/layout/HeaderLayout.tsx
@@ -3,7 +3,7 @@ import { LayoutProps } from './layoutTypes';
 
 const HeaderLayout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-1/5 shrink-0 basis-auto border-b-2 border-zinc-400 border-opacity-20">
+    <div className="h-1/6 shrink-0 basis-auto border-b-2 border-zinc-400 border-opacity-20">
       {children}
     </div>
   );
