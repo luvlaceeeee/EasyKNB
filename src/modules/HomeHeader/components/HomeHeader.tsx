b759ab4fb6c00bfc53e4a743169aef284362a69e
luvlaceeeee <nikitan2002@gmail.com>
2023-03-27 16:15:40 +0300

    create home header

--- src/modules/HomeHeader/components/HomeHeader.tsx
+++ src/modules/HomeHeader/components/HomeHeader.tsx
@@ -1,3 +1,6 @@
+import HeaderButton from '../../../shared/UI/Buttons/HeaderButton';
+import SearchBar from '../../../shared/UI/SearchBar/SearchBar';
+
 const HomeHeader = () => {
   return (
     <div className="flex h-full items-center justify-between p-7">
@@ -6,10 +9,21 @@ const HomeHeader = () => {
         <span className="text-2xl font-bold">All your project:</span>
         <span className="text-lg text-gray-400">Total project: {3}</span>
       </div>
+
       {/* Right part of header */}
-      <div className="flex space-x-3">
-        <div>Buttons</div>
-        <div>SearchBar</div>
+      <div className="flex items-center space-x-3">
+        <HeaderButton
+          text="Create board"
+          handlerFn={() => {
+            console.log('test');
+          }}
+        />
+        <SearchBar
+          placeholder="Search project"
+          handlerFn={() => {
+            console.log('test');
+          }}
+        />
       </div>
     </div>
   );
