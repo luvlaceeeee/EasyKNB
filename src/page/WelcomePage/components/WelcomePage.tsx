f266a213513d1ffc05b5911acfe7bf3df37534d9
luvlaceeeee <nikitan2002@gmail.com>
2023-03-28 09:03:37 +0300

    change welcomepage

--- src/page/WelcomePage/components/WelcomePage.tsx
+++ src/page/WelcomePage/components/WelcomePage.tsx
@@ -2,16 +2,10 @@ import { FC } from 'react';
 import { Link } from 'react-router-dom';
 
 const WelcomePage: FC = () => {
-  const handlerClick = () => {
-    localStorage.setItem('userId', '1');
-  };
-
   return (
     <div>
-      <Link to={'/space'}>
-        <button onClick={handlerClick} className="m-10 bg-slate-400 p-5">
-          log in with user id=1
-        </button>
+      <Link to={'/home'}>
+        <button className="m-10 bg-slate-400 p-5">log in with user id=1</button>
       </Link>
     </div>
   );
