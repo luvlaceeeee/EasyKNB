b5fa07b0626c13f0eb652193371df8eeaee99e59
luvlaceeeee <nikitan2002@gmail.com>
2023-03-28 07:16:59 +0300

    change homeheader

--- src/modules/HomeHeader/components/HomeHeader.tsx
+++ src/modules/HomeHeader/components/HomeHeader.tsx
@@ -5,11 +5,10 @@ const HomeHeader = () => {
   return (
     <div className="flex h-full items-center justify-between p-7">
       {/* Left part of header */}
-      <div className="inline-flex flex-col space-y-2">
-        <span className="text-2xl font-bold">All your project:</span>
-        <span className="text-lg text-gray-400">Total project: {3}</span>
+      <div className="inline-flex flex-col">
+        <span className="text-2xl font-black">All your project:</span>
+        <span className=" text-gray-400">Total project: {3}</span>
       </div>
-
       {/* Right part of header */}
       <div className="flex items-center space-x-3">
         <HeaderButton
