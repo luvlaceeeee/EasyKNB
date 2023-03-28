a475f8647cef193d5d982e4e7a09aadfac687cff
luvlaceeeee <nikitan2002@gmail.com>
2023-03-28 09:04:03 +0300

    add fake auth

---
+++ src/shared/store/AuthStore.tsx
@@ -0,0 +1,23 @@
+import { atom } from 'jotai';
+
+export interface IUser {
+  id: number;
+  email: string;
+  name: string;
+  surname: string;
+  avatar: string;
+  yandexID: string;
+  fullName: string;
+}
+
+export const userAtom = atom<IUser>({
+  id: 1,
+  email: 'eee@.ss',
+  name: 'Никита',
+  surname: 'Литвинков',
+  avatar: 'null',
+  yandexID: 'null',
+  fullName: 'Никита Литвинков',
+});
+
+export const isAuthAtom = atom(true);
