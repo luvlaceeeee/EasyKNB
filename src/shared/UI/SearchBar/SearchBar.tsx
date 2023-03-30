d50b2da7c3bc4eca0efa62bc970627338dde69f9
luvlaceeeee <nikitan2002@gmail.com>
2023-03-30 07:55:46 +0300

    fix: search bar console errors

--- src/shared/UI/SearchBar/SearchBar.tsx
+++ src/shared/UI/SearchBar/SearchBar.tsx
@@ -24,9 +24,9 @@ const SearchBar: FC<SearchBarProps> = ({ placeholder, handlerFn }) => {
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
+            // stroke-linecap="round"
+            // stroke-linejoin="round"
+            // stroke-width="2"
             d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
           ></path>
         </svg>
