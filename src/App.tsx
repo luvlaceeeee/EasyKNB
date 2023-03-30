9686ea44e23e7220db81425ffd194fe88e7c4fad
luvlaceeeee <nikitan2002@gmail.com>
2023-03-30 05:31:21 +0300

    feat: create query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
+import { QueryClient, QueryClientProvider } from 'react-query';
 import RouterPage from './page/Router/components/RouterPage';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 const App = () => {
-  return <RouterPage />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterPage />
+    </QueryClientProvider>
+  );
 };
 
 export default App;
