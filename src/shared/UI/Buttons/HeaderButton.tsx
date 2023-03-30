d0f7468ffba70e7a4e01d5f884da186b4ba50e11
luvlaceeeee <nikitan2002@gmail.com>
2023-03-30 21:06:12 +0300

    style: rename props for button

--- src/shared/UI/Buttons/HeaderButton.tsx
+++ src/shared/UI/Buttons/HeaderButton.tsx
@@ -1,20 +1,16 @@
 import { FC } from 'react';
 
 export interface HeaderButtonProps {
-  handlerFn: () => void;
+  onClick: () => void;
   text: string;
   className?: string;
 }
 
-const HeaderButton: FC<HeaderButtonProps> = ({
-  text,
-  handlerFn,
-  className,
-}) => {
+const HeaderButton: FC<HeaderButtonProps> = ({ text, onClick, className }) => {
   return (
     <button
       type="button"
-      onClick={handlerFn}
+      onClick={onClick}
       className={`rounded-xl border-2 border-gray-200 bg-white py-3 px-4 text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-gray-700 ${className}`}
     >
       {text}
