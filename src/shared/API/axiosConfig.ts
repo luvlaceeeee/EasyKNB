557217806d74205977c87be86316c198740880dd
luvlaceeeee <nikitan2002@gmail.com>
2023-03-30 07:56:38 +0300

    fix: change basic url in config

--- src/shared/API/axiosConfig.ts
+++ src/shared/API/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASIC_URL = 'http://localhost:7841';
+export const BASIC_URL = 'http://localhost:7842';
 
 const $api = axios.create({
   baseURL: BASIC_URL,
