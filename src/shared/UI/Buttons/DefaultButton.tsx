ee3b953f441d16803441effef021efb601b20ebe
luvlaceeeee <nikitan2002@gmail.com>
2023-03-31 19:06:11 +0300

    feat: add loader for button

--- src/shared/UI/Buttons/DefaultButton.tsx
+++ src/shared/UI/Buttons/DefaultButton.tsx
@@ -4,19 +4,21 @@ export interface DefaultButtonProps {
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
   text: string;
   className?: string;
+  isLoading?: boolean;
 }
 
 const DefaultButton: FC<DefaultButtonProps> = ({
   onClick,
   text,
   className,
+  isLoading,
 }) => {
   return (
     <button
       onClick={onClick}
       className={`w-full rounded-lg bg-gray-700 px-5 py-2.5 text-center text-sm font-medium text-white transition-all hover:bg-gray-800 ${className}`}
     >
-      {text}
+      {isLoading ? '...Loading' : text}
     </button>
   );
 };
