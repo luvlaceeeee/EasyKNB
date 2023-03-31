0529410d289c412426557b68c9cc2bc232429ed4
luvlaceeeee <nikitan2002@gmail.com>
2023-03-31 19:07:32 +0300

    feat: add suspense loader fix: board fetch

--- src/page/HomePage/components/HomePage.tsx
+++ src/page/HomePage/components/HomePage.tsx
@@ -1,4 +1,5 @@
 import { atomsWithQuery } from 'jotai-tanstack-query';
+import { Suspense } from 'react';
 import HomeContent from '../../../modules/HomeContent/components/HomeContent';
 import HomeHeader from '../../../modules/HomeHeader/components/HomeHeader';
 import ContentLayout from '../../../shared/layout/ContentLayout';
@@ -7,22 +8,25 @@ import { userIdAtom } from '../../../shared/store/AuthStore';
 import BoardService from '../API/HomeService';
 import { IHomeBoard } from '../types/IHomeBoard';
 
-export const [allBoards] = atomsWithQuery<IHomeBoard[]>((get) => ({
-  queryKey: ['query-boards', get(userIdAtom)],
-  queryFn: ({ queryKey: [, id] }) =>
-    BoardService.findBoardByUserId(id as number), ///TODO fix unknown type of id(delete 'as number')
-}));
+export const [allBoards] = atomsWithQuery<IHomeBoard[]>((get) => {
+  const userId = get(userIdAtom);
+
+  return {
+    queryKey: ['query-boards', userId],
+    queryFn: async () => BoardService.findBoardByUserId(userId),
+  };
+});
 
 const HomePage = () => {
   return (
-    // <Provider>
     <ContentLayout>
-      <HeaderLayout>
-        <HomeHeader />
-      </HeaderLayout>
-      <HomeContent />
+      <Suspense fallback={<h1>Loading profile...</h1>}>
+        <HeaderLayout>
+          <HomeHeader />
+        </HeaderLayout>
+        <HomeContent />
+      </Suspense>
     </ContentLayout>
-    // </Provider>
   );
 };
 
