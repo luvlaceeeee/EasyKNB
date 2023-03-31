eccc2211e3071cc1f1771911b13b908c0da9a90e
luvlaceeeee <nikitan2002@gmail.com>
2023-03-31 05:51:23 +0300

    fix: remove provider and fix sidebar

--- src/page/HomePage/components/HomePage.tsx
+++ src/page/HomePage/components/HomePage.tsx
@@ -1,4 +1,3 @@
-import { Provider } from 'jotai';
 import { atomsWithQuery } from 'jotai-tanstack-query';
 import HomeContent from '../../../modules/HomeContent/components/HomeContent';
 import HomeHeader from '../../../modules/HomeHeader/components/HomeHeader';
@@ -11,19 +10,19 @@ import { IHomeBoard } from '../types/IHomeBoard';
 export const [allBoards] = atomsWithQuery<IHomeBoard[]>((get) => ({
   queryKey: ['query-boards', get(userIdAtom)],
   queryFn: ({ queryKey: [, id] }) =>
-    BoardService.findBoardByUserId(id as number),
+    BoardService.findBoardByUserId(id as number), ///TODO fix unknown type of id(delete 'as number')
 }));
 
 const HomePage = () => {
   return (
-    <Provider>
-      <ContentLayout>
-        <HeaderLayout>
-          <HomeHeader />
-        </HeaderLayout>
-        <HomeContent />
-      </ContentLayout>
-    </Provider>
+    // <Provider>
+    <ContentLayout>
+      <HeaderLayout>
+        <HomeHeader />
+      </HeaderLayout>
+      <HomeContent />
+    </ContentLayout>
+    // </Provider>
   );
 };
 
