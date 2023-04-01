0e9b5df2220d71a7d834e82f6fd85f65ba4602ed
luvlaceeeee <nikitan2002@gmail.com>
2023-04-01 11:29:24 +0300

    fix: change backend port in axios config

--- src/shared/API/axiosConfig.ts
+++ src/shared/API/axiosConfig.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const BASIC_URL = 'http://localhost:7842';
+export const BASIC_URL = 'http://localhost:8080';
 
 const $api = axios.create({
   baseURL: BASIC_URL,
