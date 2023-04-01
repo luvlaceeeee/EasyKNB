8d1834610fff5d60ed5472a4657a2270d4ab5f57
luvlaceeeee <nikitan2002@gmail.com>
2023-04-01 11:30:45 +0300

    feat: add scroll smooth to homepage

--- src/modules/HomeContent/components/HomeContent.tsx
+++ src/modules/HomeContent/components/HomeContent.tsx
@@ -6,7 +6,7 @@ const HomeContent = () => {
   const [data] = useAtom(allBoards);
 
   return (
-    <div className="scrollbar flex flex-1 space-x-5 overflow-auto p-7 pt-0">
+    <div className="scrollbar flex flex-1 space-x-5 overflow-auto scroll-smooth p-7 pt-0">
       {data.map((board) => {
         return <BoardCard key={board.id} id={board.id} title={board.title} />;
       })}
