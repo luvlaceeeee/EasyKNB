58dad17b5c11ed124372388b180488a6282fb2b4
luvlaceeeee <nikitan2002@gmail.com>
2023-04-03 03:34:30 +0300

    feat: add rename and delete board service

--- src/page/BoardPage/API/BoardService.ts
+++ src/page/BoardPage/API/BoardService.ts
@@ -13,8 +13,41 @@ const findBoardByUserId = async (
   return data;
 };
 
+const deleteBoardById = async (
+  userId: number,
+  boardId: number | string | undefined
+) => {
+  const { data } = await $api.delete(`/boards/${boardId}`, {
+    params: {
+      userId: userId,
+    },
+  });
+  return data;
+};
+
+const renameBoardById = async (
+  userId: number,
+  boardId: number | string | undefined,
+  title: string
+) => {
+  const { data } = await $api.put(
+    `/boards/${boardId}`,
+    {
+      title: title,
+    },
+    {
+      params: {
+        userId: userId,
+      },
+    }
+  );
+  return data;
+};
+
 const BoardService = {
   findBoardByUserId,
+  renameBoardById,
+  deleteBoardById,
 };
 
 export default BoardService;
