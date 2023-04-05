b1e6d226cc0d0353bb0f52608f62d87a496af209
luvlaceeeee <nikitan2002@gmail.com>
2023-04-05 14:25:48 +0300

    fix: add whitespace for sidebar profile

--- src/modules/Sidebar/components/SidebarProfile.tsx
+++ src/modules/Sidebar/components/SidebarProfile.tsx
@@ -9,7 +9,7 @@ export const SidebarProfile = () => {
     <div className="mb-3 flex items-center justify-center rounded-lg p-2 transition-all duration-300 ease-out hover:bg-gray-200 dark:hover:bg-zinc-800">
       <img className="h-8 w-8 rounded-full" src={user.avatar} />
       <span
-        className={`pl-3 font-bold duration-500 dark:text-zinc-200 ${
+        className={`whitespace-pre pl-3 font-bold duration-500 dark:text-zinc-200 ${
           !isOpen &&
           'hidden translate-x-28 overflow-hidden opacity-0 transition-all'
         } max-w-sm`}
