ee7e2d98872d98cc4fcdbfb336d5ad60a264e3cb
luvlaceeeee <nikitan2002@gmail.com>
2023-04-06 01:37:19 +0300

    feat: add handling string in task

--- src/modules/Task/components/TaskContent.tsx
+++ src/modules/Task/components/TaskContent.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 
 export const TaskContent: FC<{ desc: string }> = ({ desc }) => {
+  const capitalizeDesc = desc.slice(0, 60) + `${desc.length > 60 && '...'}`;
   return (
     <div>
-      <span className="text-zinc-500">{desc}</span>
+      <span className="text-zinc-500">{capitalizeDesc}</span>
     </div>
   );
 };
--- src/modules/Task/components/TaskHeader.tsx
+++ src/modules/Task/components/TaskHeader.tsx
@@ -1,9 +1,10 @@
 import { FC } from 'react';
 
 export const TaskHeader: FC<{ title: string }> = ({ title }) => {
+  const capitalizeTitle = title.slice(0, 24) + `${title.length > 24 && '...'}`;
   return (
     <div className="flex justify-between">
-      <span className="text-lg font-black">{title}</span>
+      <span className="text-lg font-black">{capitalizeTitle}</span>
     </div>
   );
 };
