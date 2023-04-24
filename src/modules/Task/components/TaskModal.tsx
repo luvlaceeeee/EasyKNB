1b7285d22232c09ef75ab6002ecc165f8f9fa6d1
luvlaceeeee <nikitan2002@gmail.com>
2023-04-25 00:32:46 +0300

    feat: add fetch task

--- src/modules/Task/components/TaskModal.tsx
+++ src/modules/Task/components/TaskModal.tsx
@@ -1,25 +1,27 @@
 import { boardIdAtom } from '@page/BoardPage';
 import { URLModal } from '@shared/UI';
+import { stringToNumber } from '@shared/helpers';
 import { userIdAtom } from '@shared/store';
 import { ITask } from '@shared/types';
-import { atomsWithQuery } from 'jotai-tanstack-query';
+import { useAtomValue } from 'jotai';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
 import { TaskService } from '../API';
-export const [taskAtom] = atomsWithQuery<ITask>((get) => {
-  const userId = get(userIdAtom);
-  const boardId = get(boardIdAtom);
-
-  // const taskId = get(taskId);
-  return {
-    queryKey: ['query-task', userId, boardId, columnId, taskId],
-    queryFn: () =>
-      boardId
-        ? TaskService.findTaskById(userId, boardId, columnId, taskId)
-        : Promise.reject('Board id is null'),
-  };
-});
 export const TaskModal = () => {
+  const userId = useAtomValue(userIdAtom);
+  const boardId = useAtomValue(boardIdAtom);
+  const { columnId, taskId } = useParams();
+  const { data } = useQuery<ITask>(['task-column', taskId], () =>
+    TaskService.findTaskById(
+      userId,
+      boardId,
+      stringToNumber(columnId),
+      stringToNumber(taskId)
+    )
+  );
   return (
     <URLModal>
+      <p>{data?.text}</p>
       <div>Header</div>
       <div>Content</div>
     </URLModal>
