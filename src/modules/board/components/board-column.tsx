77246d307e7a37d5568122819eea6677324fcd97
luvlaceeeee <nikitan2002@gmail.com>
2023-06-02 00:13:35 +0300

    feat: add first dnd

--- src/modules/board/components/board-column.tsx
+++ src/modules/board/components/board-column.tsx
@@ -1,5 +1,6 @@
 import { IColumn } from '@/shared/types';
 import { FC } from 'react';
+import { DragDropContext } from 'react-beautiful-dnd';
 import { useColumnData } from '../hooks';
 import { BoardColumnContent } from './column/board-column-content';
 import { BoardColumnHeader } from './column/board-column-header';
@@ -8,15 +9,17 @@ export const BoardColumn: FC<{ column: IColumn }> = ({ column }) => {
   const { data } = useColumnData(column.id);
 
   return (
-    <div className="flex h-full w-72 shrink-0 flex-col space-y-4">
-      <BoardColumnHeader
-        title={data ? data.title : ''}
-        columnId={data ? data.id : 0}
-      />
-      <BoardColumnContent
-        columnId={data ? data.id : 0}
-        tasks={data ? data.tasks : []}
-      />
-    </div>
+    <DragDropContext onDragEnd={() => console.log('first')}>
+      <div className="flex h-full w-72 shrink-0 flex-col space-y-4">
+        <BoardColumnHeader
+          title={data ? data.title : ''}
+          columnId={data ? data.id : 0}
+        />
+        <BoardColumnContent
+          columnId={data ? data.id : 0}
+          tasks={data ? data.tasks : []}
+        />
+      </div>
+    </DragDropContext>
   );
 };
